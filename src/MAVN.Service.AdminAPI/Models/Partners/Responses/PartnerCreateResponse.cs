eacaa0c64ff5da5731f3f1099307deed11efed60
Iliyan Iliev <iliyan.iliev@primeholding.com>
2020-05-07 13:41:42 +0300

    Closes #29

---
+++ src/MAVN.Service.AdminAPI/Models/Partners/Responses/PartnerCreateResponse.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+
+namespace MAVN.Service.AdminAPI.Models.Partners.Responses
+{
+    /// <summary>
+    /// Response model for partner creation
+    /// </summary>
+    [PublicAPI]
+    public class PartnerCreateResponse
+    {
+        /// <summary>
+        /// Id of the partner
+        /// </summary>
+        public string PartnerId { get; set; }
+    }
+}
