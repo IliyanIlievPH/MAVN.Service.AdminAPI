016c69722c10e45fa1f3fecb6c078bd82d261383
Iliyan Iliev <iliyan.iliev@primeholding.com>
2020-06-15 15:52:42 +0300

    Closes #77

---
+++ src/MAVN.Service.AdminAPI/Validators/SmartVouchers/Vouchers/PresentVouchersRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MAVN.Service.AdminAPI.Models.SmartVouchers.Vouchers;
+
+namespace MAVN.Service.AdminAPI.Validators.SmartVouchers.Vouchers
+{
+    public class PresentVouchersRequestValidator : AbstractValidator<PresentVouchersRequest>
+    {
+        public PresentVouchersRequestValidator()
+        {
+            RuleForEach(x => x.CustomersEmails)
+                .NotNull()
+                .NotEmpty()
+                .EmailAddress()
+                .WithMessage("All emails must be valid");
+        }
+    }
+}
