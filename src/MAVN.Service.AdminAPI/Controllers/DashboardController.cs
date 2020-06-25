cefadc7fd6d9c29239e7aaaca34e010c8670f104
Iliyan Iliev <iliyan.iliev@primeholding.com>
2020-06-25 11:58:07 +0300

    Closes #84

--- src/MAVN.Service.AdminAPI/Controllers/DashboardController.cs
+++ src/MAVN.Service.AdminAPI/Controllers/DashboardController.cs
@@ -9,14 +9,17 @@ using MAVN.Service.DashboardStatistics.Client;
 using MAVN.Service.DashboardStatistics.Client.Models.Customers;
 using MAVN.Service.DashboardStatistics.Client.Models.Tokens;
 using MAVN.Service.AdminAPI.Domain.Enums;
+using MAVN.Service.AdminAPI.Domain.Services;
 using MAVN.Service.AdminAPI.Infrastructure;
 using MAVN.Service.AdminAPI.Infrastructure.CustomAttributes;
 using MAVN.Service.AdminAPI.Models.Dashboard;
+using MAVN.Service.CurrencyConvertor.Client;
 using MAVN.Service.DashboardStatistics.Client.Models.VoucherStatistic;
 using MAVN.Service.PartnerManagement.Client;
 using MAVN.Service.PartnerManagement.Client.Models.Partner;
 using Microsoft.AspNetCore.Mvc;
 using CustomersStatisticResponseModel = MAVN.Service.AdminAPI.Models.Dashboard.CustomersStatisticResponse;
+using VoucherDailyStatisticsModel = MAVN.Service.AdminAPI.Models.Dashboard.VoucherDailyStatisticsModel;
 using VoucherDailyStatisticsResponse = MAVN.Service.AdminAPI.Models.Dashboard.VoucherDailyStatisticsResponse;
 using VoucherStatisticsResponse = MAVN.Service.AdminAPI.Models.Dashboard.VoucherStatisticsResponse;
 
@@ -29,20 +32,25 @@ namespace MAVN.Service.AdminAPI.Controllers
     {
         private readonly IDashboardStatisticsClient _dashboardStatisticsClient;
         private readonly IPartnerManagementClient _partnerManagementClient;
+        private readonly ICurrencyConvertorClient _currencyConvertorClient;
         private readonly IMapper _mapper;
         private readonly IExtRequestContext _requestContext;
+        private readonly ISettingsService _settingsService;
 
         public DashboardController(
             IDashboardStatisticsClient dashboardStatisticsClient,
             IPartnerManagementClient partnerManagementClient,
+            ICurrencyConvertorClient currencyConvertorClient,
             IMapper mapper,
-            IExtRequestContext requestContext
-        )
+            IExtRequestContext requestContext,
+            ISettingsService settingsService)
         {
             _dashboardStatisticsClient = dashboardStatisticsClient;
             _partnerManagementClient = partnerManagementClient;
+            _currencyConvertorClient = currencyConvertorClient;
             _mapper = mapper;
             _requestContext = requestContext;
+            _settingsService = settingsService;
         }
 
         /// <summary>
@@ -66,7 +74,7 @@ namespace MAVN.Service.AdminAPI.Controllers
         {
             var (partnerIds, isEmptyResult) = await FilterByPartnerAsync();
 
-            if(isEmptyResult)
+            if (isEmptyResult)
                 return new CustomersStatisticResponseModel();
 
             var result = await _dashboardStatisticsClient.CustomersApi.GetAsync(new CustomersListRequestModel
@@ -119,17 +127,43 @@ namespace MAVN.Service.AdminAPI.Controllers
                 PermissionLevel.PartnerEdit,
             }
         )]
-        [ProducesResponseType(typeof(List<VoucherStatisticsResponse>), (int)HttpStatusCode.OK)]
-        public async Task<List<VoucherStatisticsResponse>> GetSmartVouchersStatisticsAsync()
+        [ProducesResponseType(typeof(VoucherStatisticsResponse), (int)HttpStatusCode.OK)]
+        public async Task<VoucherStatisticsResponse> GetSmartVouchersStatisticsAsync()
         {
             var (partnerIds, isEmptyResult) = await FilterByPartnerAsync();
 
+            var result = new VoucherStatisticsResponse();
+
             if (isEmptyResult)
-                return new List<VoucherStatisticsResponse>();
+                return result;
 
-            var result = await _dashboardStatisticsClient.SmartVouchersApi.GetTotalStatisticsAsync(partnerIds);
+            var statisticsForAllCurrencies = await _dashboardStatisticsClient.SmartVouchersApi.GetTotalStatisticsAsync(partnerIds);
 
-            return _mapper.Map<List<VoucherStatisticsResponse>>(result);
+            result.Currency = _settingsService.GetBaseCurrency();
+
+            foreach (var statistics in statisticsForAllCurrencies)
+            {
+                if (!statistics.Currency.Equals(result.Currency, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    var totalPurchaseCost = await _currencyConvertorClient.Converter.ConvertAsync(statistics.Currency.ToUpper(),
+                        result.Currency.ToUpper(), statistics.TotalPurchasesCost);
+                    var totalRedeemedCost = await _currencyConvertorClient.Converter.ConvertAsync(statistics.Currency.ToUpper(),
+                        result.Currency.ToUpper(), statistics.TotalRedeemedVouchersCost);
+
+                    result.TotalPurchasesCost += totalPurchaseCost.Amount;
+                    result.TotalRedeemedVouchersCost += totalRedeemedCost.Amount;
+                }
+                else
+                {
+                    result.TotalPurchasesCost += statistics.TotalPurchasesCost;
+                    result.TotalRedeemedVouchersCost += statistics.TotalRedeemedVouchersCost;
+                }
+
+                result.TotalPurchasesCount += statistics.TotalPurchasesCount;
+                result.TotalRedeemedVouchersCount += statistics.TotalRedeemedVouchersCount;
+            }
+
+            return result;
         }
 
         /// <summary>
@@ -153,24 +187,66 @@ namespace MAVN.Service.AdminAPI.Controllers
         {
             var (partnerIds, isEmptyResult) = await FilterByPartnerAsync();
 
-            if (isEmptyResult)
-                return new VoucherDailyStatisticsResponse();
+            var result = new VoucherDailyStatisticsResponse();
 
-            var result = await _dashboardStatisticsClient.SmartVouchersApi.GetPeriodStatsAsync(new VouchersDailyStatisticsRequest
+            if (isEmptyResult)
+                return result;
+
+            var dailyStatistics = await _dashboardStatisticsClient.SmartVouchersApi.GetPeriodStatsAsync(new VouchersDailyStatisticsRequest
             {
                 PartnerIds = partnerIds,
                 FromDate = request.FromDate,
                 ToDate = request.ToDate,
             });
 
-            return _mapper.Map<VoucherDailyStatisticsResponse>(result);
+            var baseCurrency = _settingsService.GetBaseCurrency();
+
+            result.BoughtVoucherStatistics = await CalculateStatisticsPerDayForBaseCurrency(dailyStatistics.BoughtVoucherStatistics, baseCurrency);
+            result.UsedVoucherStatistics = await CalculateStatisticsPerDayForBaseCurrency(dailyStatistics.UsedVoucherStatistics, baseCurrency);
+
+            return result;
+        }
+
+        private async Task<List<VoucherDailyStatisticsModel>> CalculateStatisticsPerDayForBaseCurrency
+            (IEnumerable<DashboardStatistics.Client.Models.VoucherStatistic.VoucherDailyStatisticsModel> dailyStatistics, string baseCurrency)
+        {
+            var statisticsPerDay = new Dictionary<DateTime, VoucherDailyStatisticsModel>();
+
+            foreach (var statistics in dailyStatistics)
+            {
+                decimal amount;
+                if (!statistics.Currency.Equals(baseCurrency, StringComparison.CurrentCultureIgnoreCase))
+                    amount = (await _currencyConvertorClient.Converter.ConvertAsync(statistics.Currency.ToUpper(),
+                        baseCurrency.ToUpper(), statistics.Sum)).Amount;
+                else
+                    amount = statistics.Sum;
+
+                if (statisticsPerDay.ContainsKey(statistics.Date))
+                {
+                    var current = statisticsPerDay[statistics.Date];
+                    current.Sum += amount;
+                    current.Count += statistics.Count;
+                }
+                else
+                {
+                    statisticsPerDay.Add(statistics.Date, new VoucherDailyStatisticsModel
+                    {
+                        Currency = baseCurrency,
+                        Date = statistics.Date,
+                        Count = statistics.Count,
+                        Sum = amount
+                    });
+                }
+            }
+
+            return statisticsPerDay.Values.ToList();
         }
 
         private async Task<(Guid[] PartnerIds, bool IsEmptyResult)> FilterByPartnerAsync()
         {
             var permissionLevel = await _requestContext.GetPermissionLevelAsync(PermissionType.Dashboard);
 
-            if (!permissionLevel.HasValue || permissionLevel.Value == PermissionLevel.View) 
+            if (!permissionLevel.HasValue || permissionLevel.Value == PermissionLevel.View)
                 return (null, true);
 
             var partnersResponse =
